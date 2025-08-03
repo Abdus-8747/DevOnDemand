5abbad7d173e69061a8c2a0f36cab36cbd726e56
Abdus Samad <abdussamadshamsi486@gmail.com>
2025-08-03 21:01:38 +0530

    Test Again

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ connectDB();
 // Middleware
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin:"http://localhost:5173",
     credentials: true,
   })
 );
